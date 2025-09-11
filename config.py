3159bbe6c90e0ee646ebfa99964108f4e7723c0e
Mraprguild8133 <mraprguild06@gmail.com>
2025-09-12 01:07:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-        APP_ID = os.getenv('API_ID', '')
-        APP_HASH = os.getenv('API_HASH', '')
-        BOT_TOKEN = os.getenv('BOT_TOKEN', '')
+APP_ID = os.getenv('API_ID', '',)
+APP_HASH = os.getenv('API_HASH', '')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '')
         
-        # Wasabi configuration
-        WASABI_ACCESS_KEY = os.getenv('WASABI_ACCESS_KEY', '')
-        WASABI_SECRET_KEY = os.getenv('WASABI_SECRET_KEY', '')
-        WASABI_BUCKET = os.getenv('WASABI_BUCKET', '')
-        WASABI_REGION = os.getenv('WASABI_REGION', '')
+# Wasabi configuration
+WASABI_ACCESS_KEY = os.getenv('WASABI_ACCESS_KEY', '')
+WASABI_SECRET_KEY = os.getenv('WASABI_SECRET_KEY', '')
+WASABI_BUCKET = os.getenv('WASABI_BUCKET', '')
+WASABI_REGION = os.getenv('WASABI_REGION', '')
    
-        #start message
-        START_PIC = os.environ.get("START_PIC","")
+#start message
+START_PIC = os.environ.get("START_PIC","")
