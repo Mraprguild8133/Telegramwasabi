549b2aae7158d5a4910f8b4b9f3d82aa45d55029
Mraprguild8133 <mraprguild06@gmail.com>
2025-09-12 01:28:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 
 #start message
-START_PIC = os.environ.get("START_PIC","https://raw.githubusercontent.com/Mraprguild8133/Telegramwasabi/refs/heads/main/IMG-20250911-WA0005.jpg")
+START_PIC = os.environ.get("https://raw.githubusercontent.com/Mraprguild8133/Telegramwasabi/refs/heads/main/IMG-20250911-WA0005.jpg")
